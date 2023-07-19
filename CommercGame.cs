131146d0e808cdc6a3e3bc81fa95590ec224a79a
runwolf1991 <lmcnunes@gmail.com>
2023-07-19 17:47:43 +0100

    Changes made according to ScrotoJOEUUU

--- CommercGame.cs
+++ CommercGame.cs
@@ -15,18 +15,22 @@ namespace CommercGame
             Console.WriteLine("Commerc\n\nNavigating the world on Command Line! \n\nWhat is your name, traveller?\n\n");
             var name = Console.ReadLine();
             player = new Player(name);
-            Console.WriteLine($"Hi {name}!\n So you can start your journey, we have given you 500 coins of the finest gold!\n Why not buy a ship so you can travel further?");
+            Console.WriteLine($"\nHi {name}!\nSo you can start your journey, we have given you 500 coins of the finest gold!\nWhy not buy a ship so you can travel further?");
+           
+            
             while (isGameRunning)
             {
-                Console.WriteLine("Which ship would you like to buy?");
-                Console.WriteLine("1 - Rubber Dinghy 100$. Can take up to 100Kg of merchandise.\n2 - Jon Boat 250$. Can take up to 200Kg of merchandise.\n3 - Pontoon Boat 350$. Can take up to 400Kg of merchandise.");
+                Console.WriteLine("\nWhich ship would you like to buy?");
+                Console.WriteLine("\n1 - Rubber Dinghy 100$. Can take up to 100Kg of merchandise.\n2 - Jon Boat 250$. Can take up to 200Kg of merchandise.\n3 - Pontoon Boat 350$. Can take up to 400Kg of merchandise.");
 
                 var Choice = (Console.ReadLine());
                 int i;
                 bool IsNumeric = int.TryParse(Choice, out i);
-                if (!IsNumeric)
+                while (!IsNumeric)
                 {
                     Console.WriteLine("Please type the correct option");
+                    Console.ReadLine();
+
                     return;
                 }
 
@@ -50,60 +54,45 @@ namespace CommercGame
                         }
                         break;
 
-                        //case 2:
-                        //    if (Player.money < ShipPrice)
-                        //    {
-                        //        Console.WriteLine("You cannot afford this boat at the moment!");
-                        //        Console.ReadLine();
-                        //        BuyShip();
-                        //        break;
+                     case 2:
+                            shipType = "Jon Boat";
+                            shipSize = 200;
+                            shipPrice = 250.0;
+                            if (shipPrice < player.Money)
+                            {
+                                 var money = player.Money - shipPrice;
+                                 player.SetMoney(money);
+                                 player.SetPShip(shipType);
+                                 Console.WriteLine($"AH! A Jon Boat... This one is in good condition. Happy travelling!\nYou paid 250$ for the Boat. You have {player.Money}$ left");
+                            }
+                        else
+                        {
+                            Console.WriteLine("You cannot afford this boat at the moment!");
+                        }
+                        break;
 
-                        //    }
-                        //    else
-                        //        ShipType = "Jon Boat";
-                        //    ShipSize = 200;
-                        //    ShipPrice = 250.0;
-                        //    Player.money -= ShipPrice;
-                        //    Player.PShip = "Jon Boat";
-                        //    Console.WriteLine($"AH! A Jon Boat... This one is in good condition. Happy travelling!\nYou paid 250$ for the Boat. You have {Player.money}$ left");
-                        //    Console.ReadLine();
-                        //    break;
+                     case 3:
+                            shipType = "Pontoon Boat";
+                            shipSize = 400;
+                            shipPrice = 350.0;
+                            if (shipPrice < player.Money)
+                            {
+                            var money = player.Money - shipPrice;
+                            player.SetMoney(money);
+                            player.SetPShip(shipType);
+                            Console.WriteLine($"A fine choice! This one got loads of storage!\nYou paid 350$ for the Boat. You have {player.Money}$ left");
 
-                        //case 3:
-                        //    if (Player.money < ShipPrice)
-                        //    {
-                        //        Console.WriteLine("You cannot afford this boat at the moment!");
-                        //        Console.ReadLine();
-                        //        BuyShip();
-                        //        break;
-                        //    }
-                        //    else
-                        //        ShipType = "Pontoon Boat";
-                        //    ShipSize = 400;
-                        //    ShipPrice = 350.0;
-                        //    Player.money -= ShipPrice;
-                        //    Player.PShip = "Pontoon Boat";
-                        //    Console.WriteLine($"A fine choice! This one got loads of storage!\nYou paid 350$ for the Boat. You have {Player.money}$ left");
-                        //    Console.ReadLine();
-                        //    break;
-
+                        }
+                        else
+                        {
+                            Console.WriteLine("You cannot afford this boat at the moment!");
+                        }
+                        break;
                 }
-                // Game starts and asks player for name as an input. Stores player name in variable "name"
-                //Introducts player to BuyShip() function and ahows current player statistics after buying boat. (Name, Money left, Ship Type)
-                //Ship ship = new Ship();
-                //ship.BuyShip();
-                //Player.PlayerStats();
-                //Console.ReadLine();
+                player.PlayerStats();
+
 
             }
-
-
-
-
-
-
-
-
         }
     }
 }
